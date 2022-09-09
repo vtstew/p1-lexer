af6e24e1b3c6faa45a6a2219a24ea9c8e3e67e03
Vivian Stewart <vtstew01@gmail.com>
2022-09-09 16:11:07 -0400

    stuck

--- src/p1-lexer.c
+++ src/p1-lexer.c
@@ -13,31 +13,40 @@ TokenQueue* lex (char* text)
     Regex* keyword = Regex_new("^(def|if|else|while|return|break|continue|int|bool|void|true|false)");
     Regex* reserved = Regex_new("^(for|callout|class|interface|extends|implements|new|this|string|float|double|null)");
     Regex* whitespace = Regex_new("^[ \n]");
-    Regex* letter = Regex_new("^[a-zA-Z][a-zA-Z]*([0-9]*[a-zA-Z]*_*)*");
+    Regex* letter = Regex_new("^[a-zA-Z][a-zA-Z]*([0-9]|[a-zA-Z]|_)*");
     Regex* numbers = Regex_new("^0|^[1-9]+");
     Regex* grouping = Regex_new("\\(|\\)|\\{|\\}|\\[|\\]|\\,|\\;");
     Regex* symbols = Regex_new("\\+|\\*|=");
     Regex* or_equal = Regex_new("(<|>|=|!)=");
     Regex* strings = Regex_new("^\"([a-zA-Z]|[0-9])*\"$");
     Regex* hex = Regex_new("^(0x)([0-9]|[a-f])*");
+    Regex* comment = Regex_new("^\\/\\/");
 
 
     int line_count = 1;
     /* read and handle input */
     char match[MAX_TOKEN_LEN];
+    
     while (*text != '\0') {
- 
+        // printf("%c", *text);
         /* match regular expressions */
         if (Regex_match(whitespace, text, match)) {
             /* ignore whitespace */
+        } else if (Regex_match(comment, text, match)) {
+            while (*text != '\n'){
+                text += 1;
+            }
+            text--;
         } else if (Regex_match(reserved, text, match)) {
             Error_throw_printf("Invalid token!\n");
-        } else if (Regex_match(keyword, text, match)) {
-            TokenQueue_add(tokens, Token_new(KEY, match, line_count)); 
         } else if (Regex_match(hex, text, match)) {
             TokenQueue_add(tokens, Token_new(HEXLIT, match, line_count)); 
         } else if (Regex_match(letter, text, match)) {
-            TokenQueue_add(tokens, Token_new(ID, match, line_count)); 
+            if (Regex_match(keyword, text, match) ) {
+                TokenQueue_add(tokens, Token_new(KEY, match, line_count)); 
+            } else {
+                TokenQueue_add(tokens, Token_new(ID, match, line_count)); 
+            }
         } else if (Regex_match(numbers, text, match)) {
             TokenQueue_add(tokens, Token_new(DECLIT, match, line_count)); 
         } else if (Regex_match(or_equal, text, match)) {
